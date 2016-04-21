ae5b5436d4247b4a4bbf6550a4b928c61f098787
tcspal5892 <tcspal5892@yahoo.com>
2016-04-21 14:03:46 -0400

    Added a login and register page.  Also the necessary security measures to enforce logins.  Still needs a little work.

---
+++ SnowCrashBank/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using SnowCrashBank.App_Start;
+
+[assembly: OwinStartupAttribute(typeof(SnowCrashBank.Startup))]
+
+namespace SnowCrashBank
+
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            //ConfigureAuth(app);
+        }
+    }
+}
