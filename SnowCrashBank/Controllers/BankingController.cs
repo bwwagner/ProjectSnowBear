47201de46ee1b9b77066a7eeea2ead562fccdcfd
tcspal5892 <tcspal5892@yahoo.com>
2016-04-21 13:54:27 -0400

    Login part 3

--- SnowCrashBank/Controllers/BankingController.cs
+++ SnowCrashBank/Controllers/BankingController.cs
@@ -9,25 +9,28 @@ namespace SnowCrashBank.Controllers
 {
     public class BankingController : Controller
     {
-
+        [Authorize]
         public ActionResult Deposit()
         {
             ViewBag.Message = "Deposit page";
             return View();
         }
 
+        //[Authorize]
         public ActionResult Withdraw()
         {
             ViewBag.Message = "Withdraw page";
             return View();
         }
 
+        [Authorize]
         public ActionResult Transfer()
         {
             ViewBag.Message = "Transfer page";
             return View();
         }
 
+        [Authorize]
         public ActionResult Success()
         {
             ViewBag.Message = "Success page";
